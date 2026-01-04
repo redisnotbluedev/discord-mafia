f4dedba7b7d9d9ecf1354708f72a7de59d10de74
redisnotbluedev <ededmaude@gmail.com>
2026-01-04 16:42:06 +1030

    add joining

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord, os, logging
+import discord, os, logging, asyncio
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,29 +8,56 @@ intents.message_content = True
 bot = discord.Client(intents=intents)
 logger = logging.getLogger(__name__)
 
+players = []
+
 class StartGameView(discord.ui.View):
 	def __init__(self):
 		super().__init__(timeout=300)
 
 	@discord.ui.button(label="Play", style=discord.ButtonStyle.primary)
-	async def run_cmd(self, interaction: discord.Interaction, button: discord.ui.Button):
-		await interaction.response.send_message("Starting game...")
+	async def start_game(self, interaction: discord.Interaction, button: discord.ui.Button):
+		embed: discord.Embed = discord.Embed(
+			title="AI Plays Mafia",
+			description="The series by Turing Games, now as a Discord bot!",
+			color=discord.Color.green()
+		)
+		#players.append(interaction.user)
+		embed.add_field(name="Players", value="No players yet")#f"- {interaction.user.display_name or interaction.user.name}", inline=False)
+		await interaction.response.edit_message(embed=embed, view=JoinGameView())
+
+class JoinGameView(discord.ui.View):
+	def __init__(self):
+		super().__init__(timeout=300)
+
+	@discord.ui.button(label="Join", style=discord.ButtonStyle.secondary)
+	async def join_game(self, interaction: discord.Interaction, button: discord.ui.Button):
+		if interaction.user in players:
+			await interaction.response.send_message("You're already in the game!", ephemeral=True)
+			return
+
+		embed: discord.Embed = discord.Embed(
+			title="AI Plays Mafia",
+			description="The series by Turing Games, now as a Discord bot!",
+			color=discord.Color.green()
+		)
+		players.append(interaction.user)
+		embed.add_field(name="Players", value="\n".join([f"- {u.display_name or u.name}" for u in players]))
+		await interaction.response.edit_message(embed=embed)
 
 @bot.event
 async def on_ready():
-	logger.info("hello world")
+	logger.info(f"Logged in as {bot.user}!")
 
 @bot.event
 async def on_message(message: discord.Message):
 	if message.author == bot.user:
 		return
-
-	embed: discord.Embed = discord.Embed(
+	
+	await message.channel.send(embed=discord.Embed(
 		title="AI Plays Mafia",
 		description="The series by Turing Games, now as a Discord bot!",
 		color=discord.Color.blurple()
-	)
-	await message.channel.send(embed=embed, view=StartGameView())
+	), view=StartGameView())
 
 TOKEN = os.getenv("TOKEN")
 bot.run(TOKEN, root_logger=True)
