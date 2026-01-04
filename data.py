1f51b7e60fc9453c7b304b057653f13b3e339df2
redisnotbluedev <red@redisnot.blue>
2026-01-05 09:18:46 +1030

    bugfixes

--- data.py
+++ data.py
@@ -1,6 +1,9 @@
-import json
+import json, logging
+
+logger = logging.getLogger(__name__)
 
 def save(data: dict):
+	logger.info("Saving data %s to disk..." % json.dumps(data))
 	with open("data.json", "w") as f:
 		json.dump(data, f)
 
