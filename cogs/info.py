1a7966eee9a32b51b729b2545c76f93727a73522
redisnotbluedev <red@redisnot.blue>
2026-01-05 17:59:45 +1030

    info command

--- cogs/info.py
+++ cogs/info.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 from discord import app_commands
-import discord
+import discord, platform, psutil
 
 class InfoCog(commands.Cog):
 	def __init__(self, bot: commands.Bot):
@@ -13,3 +13,24 @@ class InfoCog(commands.Cog):
 	@app_commands.command(name="echo", description="Say something as the bot")
 	async def echo(self, interaction: discord.Interaction, text: str):
 		await interaction.response.send_message(text)
+	
+	@app_commands.command(name="info", description="View bot information and stats")
+	async def info(self, interaction: discord.Interaction):
+		embed = discord.Embed(
+			title="Info"
+		)
+		embed.add_field(name="System", value=f"""
+			OS version: `{platform.platform()}`
+			Python version: `{platform.python_version()}`
+			discord.py version: `{discord.__version__}`
+			CPU usage: `{psutil.cpu_percent(interval=1)}%`
+			RAM usage: `{psutil.virtual_memory().percent}%`
+		""")
+		embed.add_field(name="Credits", value=f"""
+			Original series by <@1454202276473340071> ([YouTube](https://www.youtube.com/@turing_games))
+			Bot adaption by <@1337909802931716197>
+			Icons from [lucide.dev](https://lucide.dev)
+			[GitHub](https://github.com/redisnotbluedev/discord-mafia)
+		""")
+
+		await interaction.response.send_message(embed=embed, allowed_mentions=discord.AllowedMentions.none)
