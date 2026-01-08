929663039e743eebd34b70776ce57627e6820c51
redisnotbluedev <ededmaude@gmail.com>
2026-01-08 15:05:44 +1030

    sync globally

--- main.py
+++ main.py
@@ -25,20 +25,14 @@ async def on_ready():
 async def setup_hook():
 	await bot.add_cog(ModerationCog(bot))
 	await bot.add_cog(InfoCog(bot))
-	logger.info("all global tree cmds: %s", [c.name for c in bot.tree.get_commands()])
 
 	profiles = data.load().get("profiles", {})
 	for channel in profiles:
 		bot.abstractors.append(GameAbstractor(int(channel), bot))
 	logger.info("Loading game abstractors, total %i", len(bot.abstractors))
 
-	guild = discord.Object(id=1457229133632241725)
-	bot.tree.copy_global_to(guild=guild)
-	synced = await bot.tree.sync(guild=guild)
-	logger.info("synced: %s", [c.name for c in synced])
-
-	cmds = bot.tree.get_commands(guild=guild)
-	logger.info("tree commands: %s", [c.name for c in cmds])
+	synced_global = await bot.tree.sync()
+	logger.info("synced globally: %s", [c.name for c in synced_global])
 
 @bot.event
 async def on_message(message: discord.Message):
