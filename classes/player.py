d9dba4f8028fbcd2b9eedcbc7ccdc905d49dced0
redisnotbluedev <red@redisnot.blue>
2026-01-27 11:37:51 +1030

    add new models courtesy of redisnotblueai

--- classes/player.py
+++ classes/player.py
@@ -4,33 +4,33 @@ from classes.roles import *
 models = [
 	{
 		"model": "gpt-4o",
-		"name": "Gemini",
-		"avatar": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Google_Gemini_icon_2025.svg/960px/Google_Gemini_icon_2025.svg.png"
+		"name": "ChatGPT 4o",
+		"avatar_url": "https://media.discordapp.net/attachments/1460808917289533601/1465512821285453964/chatgpt4o.png?ex=697960ac&is=69780f2c&hm=0f8f102918d95575239072772e01b0ee33b5a50cdccf077db0bd1ec45ba5985a&animated=true"
 	},
 	{
-		"model": "gpt-4o",
-		"name": "ChatGPT",
-		"avatar_url": "https://cdn.discordapp.com/guilds/1449548216931586260/users/561150467552444436/avatars/175173b9270c081cc1e04fad961231a8.webp?size=128"
+		"model": "deepseek-3.2",
+		"name": "DeepSeek 3.2",
+		"avatar": "https://media.discordapp.net/attachments/1460808917289533601/1465512822468251729/deepseek.png?ex=697960ac&is=69780f2c&hm=7cfdec6b45dd87eb5ec9e2d63b08f96e72dc9a305a8a6aef9cd9990a36e98b30&animated=true"
 	},
 	{
-		"model": "gpt-4o",
-		"name": "DeepSeek",
-		"avatar": "https://cdn.discordapp.com/avatars/1292256566518747219/2081952934f3212a5086fb38fa1f91d7.webp?size=128"
+		"model": "kimi-k2",
+		"name": "Kimi K2",
+		"avatar": "https://media.discordapp.net/attachments/1460808917289533601/1465512820811632680/Designer_1.png?ex=697960ac&is=69780f2c&hm=3ce244df7838a40eb92662f1c00944fa1a9914efec7220214608001d68922881&animated=true"
+	},
+	{
+		"model": "llama-4-maverick",
+		"name": "Llama 4",
+		"avatar": "https://media.discordapp.net/attachments/1460808917289533601/1465512511116939284/llama.png?ex=69796062&is=69780ee2&hm=be5ae39c391c68842ec88312b06bf3ff68a83738172fa0109cc95bd584639731&animated=true"
+	},
+	{
+		"model": "qwen3-next-80b-a3b",
+		"name": "Qwen 3",
+		"avatar": "https://media.discordapp.net/attachments/1460808917289533601/1465512510785585303/qwen.png?ex=69796062&is=69780ee2&hm=bccbd1ea7ea76bc0b6ff7f7eb7f8a1b81bb510fd67fbf47ff4b2b7cf98fb7a51&animated=true"
 	},
 	{
 		"model": "mistral-large-3",
-		"name": "Claude",
-		"avatar": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Claude_AI_symbol.svg/960px-Claude_AI_symbol.svg.png"
-	},
-	{
-		"model": "mistral-large-3",
-		"name": "Grok",
-		"avatar": "https://cdn.discordapp.com/avatars/1457791629120372747/f4839d9d3feb47c0ca071254f3a9c88b.webp?size=128"
-	},
-	{
-		"model": "mistral-large-3",
-		"name": "Kimi",
-		"avatar": "https://cdn.discordapp.com/avatars/1394070345308311662/4fa6961ff5e7aa15c662675da2e4b1e9.webp?size=128"
+		"name": "Mistral 3",
+		"avatar": "https://media.discordapp.net/attachments/1460808917289533601/1465512511838355656/mistral.png?ex=69796062&is=69780ee2&hm=bc637a4480fd2bc2e3eded35f3133eb840cf19c99dc3a3bc2e8b2d59e2c080aa&animated=true"
 	}
 ]
 
