d1aa93bd4433a77b9d6c66a45e4947acd7577521
redisnotbluedev <ededmaude@gmail.com>
2026-02-01 14:00:23 +1030

    add traceback & remove timestamp

--- logging_utils.py
+++ logging_utils.py
@@ -5,6 +5,7 @@ class WebhookLoggingHandler(logging.Handler):
 	def __init__(self, webhook: Webhook, level=logging.NOTSET):
 		super().__init__(level)
 		self.webhook = webhook
+		self.setFormatter(logging.Formatter("%(message)s\n%(exc_text)s"))
 
 	def emit(self, record):
 		try:
@@ -17,7 +18,7 @@ class WebhookLoggingHandler(logging.Handler):
 			await self.webhook.send(embed=Embed(
 				colour={logging.DEBUG: Colour.dark_grey(), logging.INFO: Colour.blurple(), logging.WARNING: Colour.orange(), logging.ERROR: Colour.red(), logging.CRITICAL: Colour.dark_red()}.get(record.levelno, Colour.default()),
 				title=record.levelname.title(),
-				description=record.getMessage() + f"\n-# <t:{int(record.created)}:f>"
+				description=self.format(record)
 			))
 		except Exception as e:
 			print(f"Webhook send failed: {e}")
