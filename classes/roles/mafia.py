7283f567cccea0605a3919b9d9b6170e49770f80
redisnotbluedev <red@redisnot.blue>
2026-02-26 15:41:28 +1030

    maf emoji is knife not gun smh

--- classes/roles/mafia.py
+++ classes/roles/mafia.py
@@ -3,6 +3,7 @@ from classes.roles import KillRole, Alignment
 class Mafia(KillRole):
 	def __init__(self, skippable: bool = False):
 		super().__init__("Mafia", Alignment.MAFIA, 'a **Mafia**.\n> Your goal is to eliminate all members of the Town. Work with your fellow Mafia members to choose a target each night and avoid suspicion during the day.', 'Can kill one player each night with their team.', skippable=skippable)
+		self.emoji = "🔪"
 
 	def is_special(self):
 		return False
